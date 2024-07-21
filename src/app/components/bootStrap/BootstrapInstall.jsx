c23f150f819c946e996502928dc9fc0238a986f1
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-21 18:37:50 +0530

    Update BootstrapInstall.jsx

--- src/app/components/bootStrap/BootstrapInstall.jsx
+++ src/app/components/bootStrap/BootstrapInstall.jsx
@@ -3,8 +3,8 @@ import React,{useEffect} from 'react'
 
 export default function BootstrapInstall() {
     useEffect(()=>{
-        require('bootstrap/dist/js/bootstrap.min.js');
-        
+        // require('bootstrap/dist/js/bootstrap.min.js');
+        typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null
     },[])
   return null
 }
