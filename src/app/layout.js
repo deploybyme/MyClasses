37cf23355e7ae7fd21c49dd08e928f00a5cafcd6
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-21 18:17:48 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import './globals.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import BootstrapInstall from "./components/bootstrap/BootstrapInstall";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <BootstrapInstall/>
       <body className={inter.className}> 
         {children}
         </body>
