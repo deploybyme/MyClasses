3f8e9ea45cc8edd4cdd50dea53e1a72c562e736d
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-21 17:37:39 +0530

    init

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import AdminLogin from "./components/Auth/Login/AdminLogin";
 export default function LoginAuth() {
 
   return (
-    <>
-    <AdminLogin/>
-    </>
+        <AdminLogin/>
   );
 }
+  
