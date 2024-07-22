0d6d02af9fc23a15e376ddb03337aa628fd96975
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-22 23:06:31 +0530

    Update StudentExpence.jsx

--- src/app/components/Profile/Student/StudentExpence.jsx
+++ src/app/components/Profile/Student/StudentExpence.jsx
@@ -84,7 +84,7 @@ const update = async ()=>{
             {
             return(
             
-              <div key={data.id} className="accordion shadow-0 mt-1">
+              <div key={data.id} className="accordion shadow-0 mt-1 overflow-hidden">
                 <div className="accordion-item ">
                   <h2 className="accordion-header">
                     <div className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#${data.id}StudentDetails`} aria-expanded="false" aria-controls="collapseTwo">
