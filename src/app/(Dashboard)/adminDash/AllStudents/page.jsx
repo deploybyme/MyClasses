439b99ac2c79ff718a96a78f45f69d2fa20a7980
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-22 23:20:51 +0530

    Update page.jsx

--- src/app/(Dashboard)/adminDash/AllStudents/page.jsx
+++ src/app/(Dashboard)/adminDash/AllStudents/page.jsx
@@ -41,7 +41,7 @@ export default function AllStudent() {
                 <div className='d-flex justify-content-center'>
                     
 
-                    <div className="spinner-border mt-5" role="status">
+                    <div className="spinner-border my-5" role="status">
                         <span className="sr-only"></span>
                     </div>
 
