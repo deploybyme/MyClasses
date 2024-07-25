d2c8cec646acf38920631d3d82dd96e58c8481d3
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-25 22:15:14 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,16 +19,16 @@ export const metadata = {
       '/apple-touch-icon.png'
     ]
   },
-  manifest:'/site.webmanifest'
+  manifest:'/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-      <meta name={'viewport'}
-       content={'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'}/>
      <BootstrapInstall/>
+    <meta name={'viewport'}
+     content={'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'}  />
     </head>
       <body className={inter.className}> 
         {children}
