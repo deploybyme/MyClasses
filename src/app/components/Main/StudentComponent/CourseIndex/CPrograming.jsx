85e1eff54189aa5ebf19598faf5d7724ebca1c64
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-27 00:36:07 +0530

    Update CPrograming.jsx

--- src/app/components/Main/StudentComponent/CourseIndex/CPrograming.jsx
+++ src/app/components/Main/StudentComponent/CourseIndex/CPrograming.jsx
@@ -3,59 +3,251 @@ import React from 'react'
 export default function CPrograming() {
   return (
     <div className='container'>
-          <h4>C Programing / C++ Index</h4>
+          <h4 className='mt-2'>Core C and C++ Index</h4>
 
-  <div class="accordion accordion-flush overflow-hidden" id="Cproraming">
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#C1" aria-expanded="false" aria-controls="flush-collapseOne">
-            C Introduction
-        </button>
-      </h2>
-      <div id="C1" class="accordion-collapse collapse" data-bs-parent="#Cproraming">
-        <div class="accordion-body">
+      <div className="accordion accordion-flush overflow-hidden mt-3" id="Cproraming">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C1" aria-expanded="false" aria-controls="flush-collapseOne">
+                C Introduction
+            </button>
+          </h2>
+          <div id="C1" className="accordion-collapse collapse" data-bs-parent="#Cproraming">
+            <div className="accordion-body">
 
 
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Getting Started with C
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Your First C Program
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  C Comments
+                </label>
+              </div>
 
+
+            </div>
         </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#C2" aria-expanded="false" aria-controls="flush-collapseTwo">
-          C Fundamentals
-        </button>
-      </h2>
-      <div id="C2" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">
-
-
-
         </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#C3" aria-expanded="false" aria-controls="flush-collapseThree">
-          C Flow Control
-        </button>
-      </h2>
-      <div id="C3" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">
-          
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed  border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C2" aria-expanded="false" aria-controls="flush-collapseTwo">
+              C Fundamentals
+            </button>
+          </h2>
+          <div id="C2" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
 
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Variables, Constants and Literals
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Data Types
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Input Output (I/O)
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Operators
+                </label>
+              </div>
 
-          
+            </div>
         </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C3" aria-expanded="false" aria-controls="flush-collapseThree">
+              C Flow Control
+            </button>
+          </h2>
+          <div id="C3" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  if...else Statements
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Switch Statements
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  goto Statement
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  while and do...while Loop
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  for Loop
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C4" aria-expanded="false" aria-controls="flush-collapseThree">
+              C++ Functions
+            </button>
+          </h2>
+          <div id="C4" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Functions
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  User-defined Functions
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C5" aria-expanded="false" aria-controls="flush-collapseThree">
+              C++ Arrays
+            </button>
+          </h2>
+          <div id="C5" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Onedimentional Arrays
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Multidimentional Arrays
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C6" aria-expanded="false" aria-controls="flush-collapseThree">
+              C++ Pointers
+            </button>
+          </h2>
+          <div id="C6" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Pointers
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  References
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C7" aria-expanded="false" aria-controls="flush-collapseThree">
+              C++ Structures
+            </button>
+          </h2>
+          <div id="C7" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Structures
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Member Function in Structures
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom" type="button" data-bs-toggle="collapse" data-bs-target="#C8" aria-expanded="false" aria-controls="flush-collapseThree">
+              C++ File Handling
+            </button>
+          </h2>
+          <div id="C8" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Opening and Closing a File
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Write to a File
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Read From a File
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  Append to a Text File
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+
       </div>
+
+
+
+
     </div>
-  </div>
-
-
-    </div>
-  </div>
-
-
-
-
-  </div>
   )
 }
