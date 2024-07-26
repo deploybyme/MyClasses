c894f1f738f0e20b77055b0d30aa99e41f7d76ed
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-07-27 00:37:12 +0530

    Update HtmlIndex.jsx

--- src/app/components/Main/StudentComponent/CourseIndex/HtmlIndex.jsx
+++ src/app/components/Main/StudentComponent/CourseIndex/HtmlIndex.jsx
@@ -2,8 +2,219 @@ import React from 'react'
 
 export default function HtmlIndex() {
   return (
-    <div>
-            html
+    <div className='container'>
+          <h4 className='mt-2'>HTML Index</h4>
+
+      <div className="accordion accordion-flush overflow-hidden mt-3" id="Cproraming">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C1" aria-expanded="false" aria-controls="flush-collapseOne">
+                Introduction
+            </button>
+          </h2>
+          <div id="C1" className="accordion-collapse collapse" data-bs-parent="#Cproraming">
+            <div className="accordion-body">
+
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  What is HTML?
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Structure
+                </label>
+              </div>
+
+
+            </div>
+        </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C2" aria-expanded="false" aria-controls="flush-collapseTwo">
+              HTML Basics
+            </button>
+          </h2>
+          <div id="C2" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Paragraphs
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Headings
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Comments
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML image
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Hyperlinks
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Lists
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Table
+                </label>
+              </div>
+
+            </div>
+        </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C3" aria-expanded="false" aria-controls="flush-collapseThree">
+              HTML Inline
+            </button>
+          </h2>
+          <div id="C3" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Block and Inline
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Bold
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Italic
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Formating
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C4" aria-expanded="false" aria-controls="flush-collapseThree">
+              HTML Form
+            </button>
+          </h2>
+          <div id="C4" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Form
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Form Elements
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Inputs
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Form Actions
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C5" aria-expanded="false" aria-controls="flush-collapseThree">
+              Semantic HTML
+            </button>
+          </h2>
+          <div id="C5" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML div Tag
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML section Tag
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML header Tag
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML footer Tag
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed border-bottom shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#C6" aria-expanded="false" aria-controls="flush-collapseThree">
+              Graphics & Media
+            </button>
+          </h2>
+          <div id="C6" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div className="accordion-body">
+              
+
+            <div className="list-group">
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Video
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Audio
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML SVG
+                </label>
+                <label className="list-group-item border-0">
+                  <input className="form-check-input rounded-pill me-1" type="checkbox" value=""/>
+                  HTML Canvas
+                </label>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
     </div>
   )
 }
