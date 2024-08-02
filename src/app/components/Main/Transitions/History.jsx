d1b7d9e7a6cba0a93497a8366ca167625dde98f0
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-08-02 16:42:31 +0530

    Update History.jsx

--- src/app/components/Main/Transitions/History.jsx
+++ src/app/components/Main/Transitions/History.jsx
@@ -47,7 +47,7 @@ export default function History() {
       <div className="alert alert-success mx-1" role="alert">
       <div className="row">
             <div className="col-12 col-md-6 mb-2 mb-md-0">Total Transition : <i className="bi bi-currency-rupee"></i> {countTransition}</div>
-            <div className="col-12 col-md-6 mt-2 mt-md-0">{(((countCollect*0.6) - countTransition)>0)?"Balance Pending":"Advance Pending"} :  <i className="bi bi-currency-rupee"></i> {(((countCollect*0.6) - countTransition)>0)?((countCollect*0.6) - countTransition) : (-((countCollect*0.6) - countTransition)) }</div>
+            <div className="col-12 col-md-6 mt-2 mt-md-0">{(((countCollect*0.6) - countTransition)>0)?"Balance Pending":"Advance"} :  <i className="bi bi-currency-rupee"></i> {(((countCollect*0.6) - countTransition)>0)?((countCollect*0.6) - countTransition) : (-((countCollect*0.6) - countTransition)) }</div>
       </div>
       </div>
 
