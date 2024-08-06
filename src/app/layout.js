2d58374de468b02b93c909772b555c5f4764758c
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-08-06 12:38:00 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import './globals.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import BootstrapInstall from "@/app/components/bootStrap/BootstrapInstall";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,11 +25,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-     <meta name={'viewport'}
-     content={'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'}/>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+      </Head>
      <BootstrapInstall/>
-    </head>
       <body className={inter.className}> 
         {children}
       </body>
