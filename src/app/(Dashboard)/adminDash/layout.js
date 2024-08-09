606ff6dc1a02a2f6d453f8ffe4460d48708c4f34
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-08-09 20:45:27 +0530

    Update layout.js

--- src/app/(Dashboard)/adminDash/layout.js
+++ src/app/(Dashboard)/adminDash/layout.js
@@ -17,10 +17,10 @@ export default function layout({ children }) {
           <NavBar Directory="AdminMember"/>
           <Offanvas Directory="AdminMember"
           tab1="All Students" icon1="bi bi-person-lines-fill" link1="/adminDash/"
-          tab2="Expense" icon2="bi bi-bank" link2="/adminDash/TotalExpence/"
-          tab3="Transition" icon3="bi bi-currency-rupee" link3="/adminDash/Transition/"
-          tab4="All Course" icon4="bi bi-journals" link4="/adminDash/AllCourse/"
-          tab5="Student Report" icon5="bi bi-clipboard-check-fill" link5="/adminDash/ReportAndCretificate/"
+          tab4="Expense" icon4="bi bi-bank" link4="/adminDash/TotalExpence/"
+          tab5="Transition" icon5="bi bi-currency-rupee" link5="/adminDash/Transition/"
+          tab2="All Course" icon2="bi bi-journals" link2="/adminDash/AllCourse/"
+          tab3="Student Report" icon3="bi bi-clipboard-check-fill" link3="/adminDash/ReportAndCretificate/"
           />
            <div className='rootdiv m-md-2 m-0 py-5 formbox'>
 
