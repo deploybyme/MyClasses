a97efa08746ab743def6401ada993a5f7f1c4364
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-08-22 09:17:15 +0530

    Update StudentExpence.jsx

--- src/app/components/Main/StudentData/StudentExpence.jsx
+++ src/app/components/Main/StudentData/StudentExpence.jsx
@@ -108,7 +108,7 @@ const update = async ()=>{
             {
             return(
             
-              <div key={data.id} className="accordion shadow-0 mt-1 overflow-hidden">
+              <div key={data.id} className="accordion shadow-0 mt-1 overflow-hidden" style={{display: (data.balance==0)? props.Balance:props.Paid}} >
                 <div className="accordion-item ">
                   <h2 className="accordion-header">
                     <div className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#${data.id}StudentDetails`} aria-expanded="false" aria-controls="collapseTwo">
