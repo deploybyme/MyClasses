f0d70adce1ce73b736aa4942a18362bf2f8aeb57
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-10-10 01:18:39 +0530

    Update CPrograming.jsx

--- src/app/components/Main/StudentComponent/CourseIndex/CPrograming.jsx
+++ src/app/components/Main/StudentComponent/CourseIndex/CPrograming.jsx
@@ -10,14 +10,13 @@ export default function CPrograming() {
     <>
 
       <a id="TopButton" href="#C/C++">
-        <button>
+        <button className="text-black">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5" />
           </svg>
         </button>
       </a>
-      
-      <h3 className="fw-bold p-2 my-3 text-light bg-dark">Core C and C++</h3>
+      <h3 id="C/C++" className="fw-bold p-2 my-3 text-light bg-dark">Core C and C++</h3>
 
       <ul className="list-group-item border-0 rounded-0 bg-dark text-light p-4" style={{lineHeight:2}}>
       <h4>INDEX</h4>
