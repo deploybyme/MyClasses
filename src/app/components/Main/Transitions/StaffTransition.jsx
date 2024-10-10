22e316b3d641be03a808f47c63e0eb057b97c1d8
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-10-10 21:08:54 +0530

    Update StaffTransition.jsx

--- src/app/components/Main/Transitions/StaffTransition.jsx
+++ src/app/components/Main/Transitions/StaffTransition.jsx
@@ -11,14 +11,15 @@ var datetime = "Paid on " + currentdate.getDate() + "/"
 + currentdate.getMinutes() + ":" 
 + currentdate.getSeconds();
 
-async function addTransition(name,Paid,studentName,PayToo){    
+async function addTransition(name,Paid,studentName,PayToo,transitionType){    
     try{
       const docRef = addDoc(collection(db,"transition"),{
         Name:name,
         Paid:(Paid),
         date:datetime,
         studentName:studentName,
-        transitionTo:PayToo
+        transitionTo:PayToo,
+        transitionType:transitionType
       });
       return true;
     } catch(error){
@@ -34,23 +35,25 @@ async function refresh(){
 }
 
 export default function StaffTransition(props) {
-    const [name,setName]=useState(props.UserMember);
+    const [name,setName]=useState();
     const [PayToo,setPayToo]=useState("Faizan Mirza");
     const[Paid,setPaid]=useState("");
     const[date,setdate]=useState(datetime);
+    const[transitionType,setTransitionType]=useState("");
     const[studentName,setstudentName]=useState("");
     
     const handleSubmit = async (e)=>{
         e.preventDefault();
-        const addData = await addTransition(name,Paid,studentName,PayToo);
+        const addData = await addTransition(name,Paid,studentName,PayToo,transitionType);
         if(addData){
           setName("")
           setPaid("")
           setdate("")
           setstudentName("")
           setPayToo("")
-          alert("Transition Save Successfully")
+          setTransitionType("")
         }
+        alert("Transition Save Successfully")
       }
        
      
@@ -63,12 +66,29 @@ export default function StaffTransition(props) {
       <form  onSubmit={handleSubmit}>
 
       <div className="row">
-            <div className="col-12 col-md-6">
-              <label htmlFor="Name" className="form-label"><b>Payee Name</b></label>
-              <input type="text" className="form-control shadow-none" readOnly
-               placeholder="Enter Student Name" id="Name" value={name || ""} 
-               />
-            </div>
+            {
+              (props.UserMember == "Faizan Mirza") ?
+              <> 
+                <div className="col-12 col-md-6">
+                  <label htmlFor="UpdatedBy" className="form-label"><b>Payee Name</b></label>
+                  <select className="form-select shadow-none" id="UpdatedBy"  required
+                  value={name}
+                  onChange={(e)=>setName(e.target.value)}
+                  >
+                    <option className='py-1' value="Amit Raghav">Amit Raghav</option>
+                    <option className='py-1' value="Ishwar Singh">Ishwar Singh</option>
+                  </select>
+                </div>
+                
+              </>:<>
+                <div className="col-12 col-md-6">
+                  <label htmlFor="Name" className="form-label"><b>Payee Name</b></label>
+                  <input type="text" className="form-control shadow-none" readOnly
+                  placeholder="Enter Student Name" id="Name" value={props.UserMember || ""} 
+                  onChange={(e)=>setName(e.target.value)}/>
+                </div>
+              </>
+            }
 
             <div className="col-12 col-md-6">
               <label htmlFor="studentName" className="form-label"><b>Against Student</b></label>
@@ -80,12 +100,29 @@ export default function StaffTransition(props) {
       </div>
 
       <div className="row mt-2">
+          
           <div className="col-12 col-md-6"> 
                   <label htmlFor="Paid" className="form-label"><b>Amount</b></label>
                   <input type="text" className="form-control shadow-none"  required
                   id="Paid" value={Paid || ""} maxLength={4} placeholder='0000'
                   onChange={(e)=>setPaid(e.target.value)}/>
           </div>
+          
+          <div className="col-12 col-md-6">
+                <label htmlFor="transitionType" className="form-label"><b>Transition Type</b></label>
+                <select className="form-select shadow-none" id="transitionType"  required
+                value={transitionType}
+                onChange={(e)=>setTransitionType(e.target.value)}
+                >
+                  <option className='py-1' value="None" >Select Cash / UPI</option>
+                  <option className='py-1' value="Cash">Cash</option>
+                  <option className='py-1' value="UPI">UPI</option>
+                </select>
+          </div>
+
+    </div>
+    <div className="row mt-2">
+
           <div className="col-12 col-md-6"> 
                   <label htmlFor="Time" className="form-label"><b>TimeStamp</b></label>
                   <input type="text" className="form-control shadow-none"  readOnly
@@ -93,7 +130,7 @@ export default function StaffTransition(props) {
                   />
           </div>
   
-      </div>
+    </div>
       <div className="row mt-3">
         <div className="col">
           <button type="submit" className="btn btn-primary" onClick={props.cardUpdate}>
