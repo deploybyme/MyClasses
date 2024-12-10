d5872be1b821b52f4451f32aff437f7720d24c12
Faizan Mirza <156444997+deploybyme@users.noreply.github.com>
2024-12-10 20:26:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,13 +20,16 @@ export const metadata = {
     ]
   },
   manifest:'/site.webmanifest',
+  viewport: {
+    content: "width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+        <meta name="viewport" content={metadata.viewport.content} />
       </Head>
      <BootstrapInstall/>
       <body className={inter.className}> 
